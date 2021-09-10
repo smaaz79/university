bf07d25d195ac10d75340bd5a65c06515a579b07
smaaz79 <89434678+smaaz79@users.noreply.github.com>
2021-09-10 15:46:46 -0700

    Delete student directory

--- student/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('add_student', views.add_student),
-    path('adding_student', views.adding_student),
-    path('view_student', views.view_student)
-]
