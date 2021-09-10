b9819c6afb5cf4a2f2c827f701ed5352b8b7583a
smaaz79 <89434678+smaaz79@users.noreply.github.com>
2021-09-10 15:45:25 -0700

    Update urls.py

--- student/urls.py
+++ student/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add_student', views.add_student),
-    path('adding_student', views.adding_student)
+    path('adding_student', views.adding_student),
     path('view_student', views.view_student)
 ]
